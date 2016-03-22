2b8f17aacb7d13bf8c4dc364189a7e5f535f374d
Miroslav Pakanec <Miroslav Pakanec>
2016-03-22 16:48:16 +0100

    order validation and server call, home icons, order design changed

--- View/JQuery/Customer/newOrder.js
+++ View/JQuery/Customer/newOrder.js
@@ -1,9 +1,7 @@
 //dynamic functionality order
 $(function(){
 
-    var timeButton = 'AM';
-
-    $('#orderDateButton').on('click', function(){
+    $('#orderDate').on('click', function(){
       $('#datepickerMain').slideToggle(500);
     }),
 
@@ -16,26 +14,10 @@ $(function(){
             maxDate: '+6M',
             onSelect: function (date) {
                     $('#orderDate').html(date);
-                  }
+                    $('#datepickerMain').slideToggle(500);
+                }
     }),
 
-    //time check
-
-    $("input[id*='orderTime']").focus(function(){
-      var curVal = $(this).val();
-      if(curVal == 'Hour' || curVal == 'Minute')
-        $(this).val('');
-    }).blur(function(){
-      var input = $(this).val();
-      if(!input.length > 0){
-        setDefaultText(this);
-        setTextColor(this, 'red');
-      }
-      else{
-       //validate if input is correct number
-       validateNumber(this, input);
-     }
-    }),
 
     //plus minut buttons
     $("#plusButton").on('click', function(){
@@ -74,7 +56,7 @@ $(function(){
     });
 
     $('#timeButtonAM').on('click', function(){
-      timeButton = 'AM';
+      $('#orderClock').val('AM');
       $(this).css({
         'background-color' : 'rgba(255, 255, 255, 0.1)'
       });
@@ -84,98 +66,12 @@ $(function(){
     }),
 
     $('#timeButtonPM').on('click', function(){
-      timeButton = 'PM';
+      $('#orderClock').val('PM');
       $(this).css({
         'background-color' : 'rgba(255, 255, 255, 0.1)'
       });
       $('#timeButtonAM').css({
         'background-color' : 'rgba(255, 255, 255, 0.0)'
       });
-    }),
-
-    $('#orderClearButton').on('click', function(){
-      $('#orderUsername').val('').css({'color' : 'black'});
-      $('#orderCompanyName').val('').css({'color' : 'black'});
-      $('#paymentTypeButton').html('Select payment');
-      $('#orderDate').html('Select date');
-      $('#datepickerMain').slideUp(500);
-      $('#orderTimeHour').val('Hour').css({'color' : 'black'});
-      $('#orderTimeMinute').val('Minute').css({'color' : 'black'});
-      $('#timeButtonAM').css({
-        'background-color' : 'rgba(255, 255, 255, 0.1)'
-      });
-      $('#timeButtonPM').css({
-        'background-color' : 'rgba(255, 255, 255, 0)'
-      });
-      timeButton = 'AM';
-      $('#ordeFrom').val('').css({'color' : 'black'});
-      $('#orderTo').val('').css({'color' : 'black'});
-      $('#orderPasangers').html('2');
     })
   });
-
-
-  function setTextColor(element, color){
-    $(element).css({
-    'color' : color
-    });
-  }
-
-  function setDefaultText(element){
-    var id = $(element).attr("id");
-
-    if(id == 'orderTimeHour')
-      $('#'+id).val('Hour');
-    else if(id == 'orderTimeMinute')
-      $('#'+id).val('Minute');
-  }
-
-  //validate if number is corretly inputed
-  function validateNumber(element, input){
-    if(!$.isNumeric(input))
-      setTextColor(element, 'red');
-    else{
-      setTextColor(element, 'black');
-      validateNegative(element, input);
-      validateAmmout(element, input);
-      validateNatural(element, input);
-    }
-  }
-
-  //validate if input is negative
-  function validateNegative(element, input){
-
-    if(input<0)
-      setTextColor(element, 'red');
-  }
-
-  //validate if ammount is correct
-  function validateAmmout(element, input){
-
-    if($(element).attr('id')=='orderTimeHour'){
-
-        if(input > 12)
-          setTextColor(element, 'red');
-
-        //check time interval
-        var d = new Date();
-        var hour = d.getUTCHours() +1;
-        validateTime(hour, input);
-    }
-
-    if($(element).attr('id')=='orderTimeMinute' && input > 60)
-            setTextColor(element, 'red');
-  }
-
-  function validateTime(hour, input){
-
-    if(hour > 20 && hour < 6){
-      //if night time allow
-    }
-  }
-
-  function validateNatural(element, input){
-
-    if(input.indexOf('.') >= 0)
-      setTextColor(element, 'red');
-  }
--- View/JQuery/Customer/newOrderLoad.js
+++ View/JQuery/Customer/newOrderLoad.js
@@ -3,19 +3,143 @@ $(function(){
     getSession(function(data){
 
       if(data.email.length == 0){
-          //window.location="mainPage.html";
+          window.location="mainPage.html";
       }
 
-      //if(data.type == 'customer'){
+      if(data.type == 'customer'){
 
         //generate page for custoemr
         generateCustomerButtons();
-      //}
+      }
     }),
 
     $('.paymentSelection').hide(0);
     $('#datepickerMain').hide();
+    $('.errorMessage').hide();
+    $('#orderClock').val('AM');
+
 })
+
+//form submit
+$(function(){
+
+    $('#orderForm').on('submit', function(e){
+
+      e.preventDefault();
+
+      if(validateOrderConfirm().length > 0)
+        return;
+
+      var url = $(this).attr('action');
+      var type = $(this).attr('method');
+      var data = {};
+      $('input[name^="order"]').each(function(index, value){
+
+        var name = $(this).attr('name');
+        var value = $(this).val();
+        data[name] = value;
+      }),
+      $('div[name^="order"]').each(function(index, value){
+
+        var name = $(this).attr('name');
+        var value = $(this).html();
+        data[name] = value;
+      }),
+
+      console.log(data)
+    })
+})
+
+//on clear
+
+$(function(){
+
+  $('#orderClearButton').on('click', function(){
+
+    $('#orderDate').html('Select date');
+    $('#datepickerMain').slideUp(500);
+    $('#orderTimeHour').val('');
+    $('#orderTimeMinute').val('');
+    $('#orderFrom').val('');
+    $('#orderTo').val('');
+    $('#paymentTypeButton').html('Select payment');
+    $('#orderPasangers').html('2');
+
+    $('.errorMessage').slideUp(500);
+  })
+})
+
+//on confirm
+function validateOrderConfirm(){
+
+  var error = '';
+  //trigger blur - to repeat automatic error check on click
+  $('input[id^="order"]').trigger('blur');
+  validateOrderButtons();
+
+  $(".errorMessage").each(function(index){
+
+    error =  error + $(this).html();
+    if($(this).html().length > 0)
+      $(this).toggle( "pulsate", 100).toggle( "pulsate", 100);
+  });
+
+  return error;
+}
+
+//on blur
+$(function(){
+
+  validateOrderTime('blur', '#orderTimeHour', '#errorOrderDepartureTime', /^[0-9+]*$/, 12, 0, 'hour');
+  validateOrderTime('blur', '#orderTimeMinute', '#errorOrderDepartureTime', /^[0-9+]*$/, 59, 0, 'minute');
+  validateOrderInput('blur', '#orderFrom', '#errorOrderFrom');
+  validateOrderInput('blur', '#orderTo', '#errorOrderTo');
+})
+
+
+function validateOrderTime(thisEvent, id, idErr, regex, max, min, name){
+
+  $(id).on(thisEvent, function(){
+    var value = $(this).val();
+
+    if(!value)
+      $(idErr).html('Cannot be empty').slideDown(500);
+    else if(value < min || value > max)
+      $(idErr).html('Incorrect '+name+' (' + min + ' - ' + max + ')').slideDown(500);
+    else if(!regex.test(value))
+      $(idErr).html('Incorrect value').slideDown(500);
+    else
+      $(idErr).html('').slideUp(500);
+  })
+}
+
+function validateOrderInput(thisEvent, id, idErr){
+
+  $(id).on(thisEvent, function(){
+    var value = $(this).val();
+
+    if(!value)
+      $(idErr).html('Cannot be empty').slideDown(500);
+    else
+      $(idErr).html('').slideUp(500);
+  })
+}
+
+function validateOrderButtons(){
+
+  validateOrderButton('#orderDate', '#errorOrderDepartureDate', 'Select date');
+  validateOrderButton('#paymentTypeButton', '#errorOrderPaymentType', 'Select payment');
+}
+
+function validateOrderButton(id, idErr, defaultValue){
+
+  value = $(id).html();
+
+  if(value == defaultValue)
+    $(idErr).html('Cannot be default').slideDown(500);
+  else
+    $(idErr).html('').slideUp(500);
+}
 
 function generateCustomerButtons(){
 
